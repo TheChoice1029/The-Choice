304622f86f2aa75f90d3f90027c0e255c73bea8c
TheChoice1029 <153381977+TheChoice1029@users.noreply.github.com>
2023-12-10 15:32:23 +0000

    Update The C-H-O-I-C-E.py
    Changed some typos and quality of life features.

--- The C-H-O-I-C-E.py
+++ The C-H-O-I-C-E.py
@@ -29,7 +29,7 @@ def speak(phrase, char):
 print('BOOTING', end='')
 phrase = '.'
 for i in range (11):
-    print(phrase[0], end='')
+    print(phrase[0], end='', flush=True)
     sleep(0.05)
 print('.')
 print('WELCOME')
@@ -37,7 +37,7 @@ sleep(0.5)
 print('Gathering files: [', end='')
 phrase= '|'
 for i in range (10):
-    print(phrase[0], end='')
+    print(phrase[0], end='', flush=True)
     sleep(0.05)
 print(']')
 print('DONE')
@@ -45,7 +45,7 @@ sleep(0.5)
 print('Applying Fonts : [', end='')
 phrase= '|'
 for i in range (10):
-    print(phrase[0], end='')
+    print(phrase[0], end='', flush=True)
     sleep(0.05)
 print(']')
 print('DONE')
@@ -53,7 +53,7 @@ sleep(0.1)
 print('Finalizing     : [', end='')
 phrase= '|'
 for i in range (10):
-    print(phrase[0], end='')
+    print(phrase[0], end='', flush=True)
     sleep(0.05)
 print(']')
 print('DONE')
@@ -65,10 +65,10 @@ print('')
 type('A text-based RPG where YOU decide what happens. ')
 sleep(0.5)
 #holy crap this looks so much cleaner than before.
-type('Your first choice is approaching, but first there is something you need to know. ')
+type('Your first choice is approaching, but first there is something you need to know.')
 print('')#This is for a new line
 sleep(0.5)
-type('There is one thing you need to keep an eye on throughout the game: ')
+type('There is one thing you need to keep an eye on throughout the game:')
 print('')
 type('''Stamina.
     Stamina represents how you are feeling and how motivated you are. Stamina is represented in percentage ('%') - 100% is high stamina, 0% is no stamina. If stamina hits 0%, then the you die. Stamina can go above 100%. You start out with 100% stamina at the start of each game. The less stamina they have, the less energy there going to have to do things with. Each choice you make will cost some stamina, so be wise but don't sacrifice too much. There will always be a task with low stamina needs if stamina is low. ''')
@@ -96,6 +96,7 @@ if location == '1':
     choice =+ 1
     print('WELCOME TO')
     type('The Mountains')
+    print('')
     invent.clear()
     invent.append('Spyglass')
     invent.append('Map')
