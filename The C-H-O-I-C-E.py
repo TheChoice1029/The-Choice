7dd5624c076cce81a7bc473d8f06748043b15880
TheChoice1029 <153381977+TheChoice1029@users.noreply.github.com>
2023-12-10 15:04:26 +0000

    Changed how 'type()' func works
    Made it auto-detect how long the phrase is and made it check if it was a '.' or a ',' so it could pause longer.

--- The C-H-O-I-C-E.py
+++ The C-H-O-I-C-E.py
@@ -2,19 +2,31 @@ from time import *
 from random import randint
 invent = []
 choice = 0
-def type(phrase, char):
+def type(phrase):
     a = 0
-    for i in range (char):
-        print(phrase[a], end='')
-        sleep(0.05)
+    for i in range (len(phrase)):
+        print(phrase[a], end='', flush=True)
+        c = phrase[a]
+        if c == '.':
+            sleep(0.5)
+        elif c == ',':
+            sleep(0.25)
+        else:
+            sleep(0.05)
         a+=1
-def speak(char):
+def speak(phrase, char):
     a = 0
     for i in range (char):
-        sleep(0.07)
-        a = a+1
-stamina = 100
-print('Booting', end='')
+        print(phrase[a], end='', flush=True)
+        c = phrase[a]
+        if c == '.':
+            sleep(0.2)
+        elif c == ',':
+            sleep(0.1)
+        else:
+            sleep(0.07)
+        a+=1
+print('BOOTING', end='')
 phrase = '.'
 for i in range (11):
     print(phrase[0], end='')
@@ -47,35 +59,35 @@ print(']')
 print('DONE')
 print('----------------------------------------------------')
 sleep(0.5)
-type('Welcome to the C-H-O-I-C-E', 26)
+type('Welcome to the C-H-O-I-C-E')
 sleep(0.5)
 print('')
-type('A text-based RPG where YOU decide what happens. ', 48)
+type('A text-based RPG where YOU decide what happens. ')
 sleep(0.5)
 #holy crap this looks so much cleaner than before.
-type('Your first choice is approaching, but first there is something you need to know. ', 81)
+type('Your first choice is approaching, but first there is something you need to know. ')
 print('')#This is for a new line
 sleep(0.5)
-type('There is one thing you need to keep an eye on throughout the game: ', 67)
+type('There is one thing you need to keep an eye on throughout the game: ')
 print('')
 type('''Stamina.
-    Stamina represents how you are feeling and how motivated you are. Stamina is represented in percentage ('%') - 100% is high stamina, 0% is no stamina. If stamina hits 0%, then the you die. Stamina can go above 100%. You start out with 100% stamina at the start of each game. The less stamina they have, the less energy there going to have to do things with. Each choice you make will cost some stamina, so be wise but don't sacrifice too much. There will always be a task with low stamina needs if stamina is low. ''', 525)
+    Stamina represents how you are feeling and how motivated you are. Stamina is represented in percentage ('%') - 100% is high stamina, 0% is no stamina. If stamina hits 0%, then the you die. Stamina can go above 100%. You start out with 100% stamina at the start of each game. The less stamina they have, the less energy there going to have to do things with. Each choice you make will cost some stamina, so be wise but don't sacrifice too much. There will always be a task with low stamina needs if stamina is low. ''')
 sleep(0.5)
 print('')
 print('-----')
-type('You have three options:', 23)
+type('You have three options:')
 sleep(0.5)
 print('')
-type('1. The Mountains [-0%]', 22)
+type('1. The Mountains [-0%]',)
 sleep(0.5)
 print('')
-type('2. The Woodland [-0%]', 21)
+type('2. The Woodland [-0%]')
 sleep(0.5)
 print('')
-type('3. The Beach [-0%]', 18)
+type('3. The Beach [-0%]')
 sleep(0.5)
 print('')
-type('Please type 1, 2 or 3 correspondent to the options.', 51)
+type('Please type 1, 2 or 3 correspondent to the options.')
 sleep(0.5)
 location = input(' ')
 location = location.upper()
@@ -83,11 +95,11 @@ if location == '1':
     print('----------------------------------------------------')
     choice =+ 1
     print('WELCOME TO')
-    type('The Mountains', 13)
+    type('The Mountains')
     invent.clear()
     invent.append('Spyglass')
     invent.append('Map')
     invent.append('Water')
     for i in range (3):
         invent.append('Apple')
-    type('There is a vast mountain.', )#INSERT CHAR NO.
+    type('There is a vast mountain.')
